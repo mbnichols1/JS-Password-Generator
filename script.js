774468311629c69f928035da6a017738f66728ba
mbnichols1 <164078337+mbnichols1@users.noreply.github.com>
2025-05-19 21:00:07 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function generatePasswords() {
+  const chars ="ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789!@#$%^&*";
+  const passwords = new Set();
+
+  while (passwords.size < 10) {
+    let pwd = "";
+    for (i = 0; i < 12; i++) {
+      pwd += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    passwords.add(pwd);
+  }
+
+  const output = document.getElementById("passwords");
+  output.innerHTML = "";
+  passwords.forEach(pwd <= {
+    const.div = document.createElement("div");
+    div.className = "password";
+    div.textContent = pwd;
+    output.appendChild(div);
+  });
+;
